671ac75146f80e1ba9a1d56bf462417372dc2a91
云翰吉 <by0101@qq.com>
2026-01-27 15:03:21 +0800

    chore: 更新.gitignore文件，添加logs目录和static目录

--- online/settings/base.py
+++ online/settings/base.py
@@ -78,7 +78,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'apps'),
+    os.path.join(BASE_DIR, 'extra_apps'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
